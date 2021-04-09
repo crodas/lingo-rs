645853abffca03b3899f1c81900e6cc334f18d90
Cesar Rodas <cesar@rodasm.com.py>
2021-04-09 16:52:06 -0400

    Added a better build.rs
    The majority of changes/improvements should be pretty much add more
    fixtures (for training and tests) and in the textcat library.

---
+++ build.rs
@@ -0,0 +1,45 @@
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
+use std::io::Write;
+use std::process::Command;
+use tera::{Context, Tera};
+use textcat::storage::learn_from_directory;
+
+#[derive(Deserialize, Serialize)]
+struct Fixture {
+    category: String,
+    fixtures: Vec<String>,
+}
+
+fn main() {
+    // train TextCat with all samples
+    let _p = learn_from_directory("./fixtures").unwrap();
+    let mut tera = Tera::default();
+
+    // load all tests
+    let file = File::open("./tests/fixtures.json").unwrap();
+    let reader = BufReader::new(file);
+    let tests: Vec<Fixture> = serde_json::from_reader(reader).unwrap();
+
+    let mut f = File::open("src/lib.tpl.rs").unwrap();
+    let mut code = String::new();
+    f.read_to_string(&mut code).unwrap();
+
+    tera.add_raw_template("tpl", &code).unwrap();
+
+    let mut context = Context::new();
+    context.insert("ngrams", &_p.to_vec());
+    context.insert("languages", &_p.categories());
+    context.insert("version", &env!("CARGO_PKG_VERSION").to_string());
+    context.insert("tests", &tests);
+
+    File::create("src/lib.rs")
+        .unwrap()
+        .write_all(tera.render("tpl", &context).unwrap().as_bytes())
+        .unwrap();
+
+    Command::new("cargo").arg("fmt").output().unwrap();
+}
